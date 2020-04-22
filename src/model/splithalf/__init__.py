3070a1c8bad9755d0b20c4c933d27be8d3c64efb
ChristianGerloff <christian.gerloff@rwth-aachen.de>
2020-04-23 00:37:04 +0200

    add: docstring

--- src/model/splithalf/__init__.py
+++ src/model/splithalf/__init__.py
@@ -1,2 +1,10 @@
+"""split half analysis module
+
+This module allows the perform a split half analysis.
+
+This script requires that `pandas`, `numpy` and `scipy` is
+installed within you environment.
+
+"""
 from .splithalf import simple_split_half
 from .pearson_brown import *
--- src/model/splithalf/splithalf.py
+++ src/model/splithalf/splithalf.py
@@ -3,14 +3,16 @@ import pandas as pd
 
 
 def simple_split_half(data: pd.DataFrame, split: float = 0.5, seed: int = 19700421) -> pd.DataFrame:
+    """Segment the data frame into two sub sets."""
+
     n_sample = np.round((1-split) * len(data)).astype(int)
 
-    #np.random.seed(seed)
+    # np.random.seed(seed)
     h1_idx = np.random.choice(data.index.values,
-                           size=n_sample, replace=False)
+                              size=n_sample, replace=False)
 
     h1_subsample = data.drop(index=h1_idx)
-    h2_subsample = data.loc[h1_idx,:]
+    h2_subsample = data.loc[h1_idx, :]
 
     # Postprocessing
     h1_subsample['subsample'] = 's1'
